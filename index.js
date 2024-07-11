e619e365bdb8946386f385439432a72719a8f5f7
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 20:16:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// Your code here// Your code here
+const moveDodgerLeft = () => {
+	let dodger = document.getElementById("dodger");
+	let left = parseInt(dodger.style.left) || 0; // Parse integer value, default to 0 if NaN
+
+
+	if (left > 0) {
+		dodger.style.left = `${left -1}px`;
+  }
+  else{dodger.style.left = `${left - 2}px`;}
+};
+
+
+moveDodgerLeft();
+
+const moveDodgerRight = () => {
+	let dodger = document.getElementById("dodger");
+	let left= parseInt(dodger.style.left || 0); // Parse integer value, default to 0 if NaN
+
+	
+	if (left > 0) {
+		dodger.style.left = `${left +1}px`;
+  }
+  
+};
+
+document.addEventListener('keydown', (e) => { if (e.key==='ArrowLeft') {moveDodgerLeft
+  
+}})
+
+
